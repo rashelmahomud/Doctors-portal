6bf7c4fd5d11298f30ef9788fe7e73aefa83fd6d
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-05-10 02:43:10 +0600

    Added all setup navber

---
+++ src/Components/Shared/Home/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import chair from '../../../assets/images/chair.png';
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold">Your New Smile Starts Here!</h1>
+                    <p class="py-6">Doctor portal is a simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.</p>
+                    <button class="btn btn-primary text-white">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
